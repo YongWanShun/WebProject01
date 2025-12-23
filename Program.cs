4215a2d6172d8ef4511fa6cb9dff2eadeaf5e463
yishin <yishin@gmail.com>
2025-12-23 18:57:01 +0800

    修改 Program

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@ namespace WebProject
             builder.Services.AddDbContext<Models.WebProjectContext>();
 
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Users/Login";       // 沒登入時，導向登入頁 (雖然我們用彈窗，但這是保險)
-        options.AccessDeniedPath = "/Home/Index"; // 【權限不足】時 (例如一般人想進後台)，踢回首頁
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(1); // 登入狀態保持 7 天
-    });
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Users/Login";       // 沒登入時，導向登入頁 (雖然我們用彈窗，但這是保險)
+                options.AccessDeniedPath = "/Home/Index"; // 【權限不足】時 (例如一般人想進後台)，踢回首頁
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(1); // 登入狀態保持 7 天
+            });
 
             var app = builder.Build();
 
